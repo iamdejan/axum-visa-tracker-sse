00395152dec2e558ea863fe060e937dee2fa6f77
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2025-05-31 13:04:37 +0700

    feat: Server-Sent Events from POST endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
 use std::{convert::Infallible, path::PathBuf, time::Duration};
 
-use axum::{response::{sse::Event, Sse}, routing::{get, get_service}, Router};
+use axum::{extract::State, response::{sse::Event, Sse}, routing::{get, get_service}, Router};
 use axum_extra::TypedHeader;
-use tokio_stream::StreamExt as _;
-use futures_util::stream::{self, Stream};
+use futures_util::stream::Stream;
+use serde::Deserialize;
+use tokio::sync::broadcast;
 use tower_http::{services::ServeFile, trace::TraceLayer};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+type AppEvent = String;
+
+#[derive(Clone)]
+pub struct AppState {
+    tx: broadcast::Sender<AppEvent>,
+}
+
+impl AppState {
+    pub fn new() -> Self {
+        let (tx, _rx) = broadcast::channel(800);
+        return Self { tx: tx };
+    }
+}
+
+#[derive(Deserialize, Debug)]
+pub struct PostEventPayload {
+
+}
 
 #[tokio::main]
 async fn main() {
@@ -30,21 +50,39 @@ fn app() -> Router {
     let assets_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("assets");
     let static_files_service = ServeFile::new(assets_dir.clone().join("index.html"));
     let fallback_service = ServeFile::new(assets_dir.clone().join("fallback.html"));
+
+    let app_state = AppState::new();
+
     return Router::new()
         .route("/sse", get(sse_handler))
         .route("/", get_service(static_files_service))
         .fallback_service(fallback_service)
-        .layer(TraceLayer::new_for_http());
+        .layer(TraceLayer::new_for_http())
+        .with_state(app_state);
 }
 
 async fn sse_handler(
+    State(state): State<AppState>,
     TypedHeader(user_agent): TypedHeader<headers::UserAgent>,
 ) -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
     tracing::debug!("{} connected", user_agent.as_str());
 
-    let stream = stream::repeat_with(|| Event::default().data("hi"))
-        .map(Ok)
-        .throttle(Duration::from_secs(1));
+    let mut rx = state.tx.subscribe();
+
+    let stream = async_stream::stream! {
+        loop {
+            match rx.recv().await {
+                Ok(msg) => {
+                    let event = Event::default().data(msg);
+                    yield Ok(event);
+                }
+                Err(err) => {
+                    tracing::error!("Error: {}", err);
+                    break;
+                }
+            }
+        }
+    };
 
     return Sse::new(stream).keep_alive(
         axum::response::sse::KeepAlive::new()
